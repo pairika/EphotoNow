63df5b3c7e89fe079b6c67784c60985c5ae1be99
pouyakhast <pkhast@yahoo.com>
2016-04-22 11:56:18 +0000

    Added credit card fields to the signup form

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,21 +1,22 @@
 class Payment < ActiveRecord::Base
-    
+
   attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
   belongs_to :user
-    
+
   def self.month_options
-    Data::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1]}  
+    Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1]}
   end
-  
+
   def self.year_options
-    (Data.today.year..(Data.today.year+10)).to_a
+    (Date.today.year..(Date.today.year+10)).to_a
   end
-  
+
   def process_payment
     customer = Stripe::Customer.create email: email, card: token
     Stripe::Charge.create customer: customer.id,
-                          amount: 1000,
-                          description: 'Premium',
-                          currency: 'usd'
+    amount: 1000,
+    description: 'Premium',
+    currency: 'usd'
   end
+
 end
