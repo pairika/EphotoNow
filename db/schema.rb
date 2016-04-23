1aaa056509113cec83e0bd7ff2b9fe0b99134b9c
pouyakhast <pkhast@yahoo.com>
2016-04-23 09:14:59 +0000

    Created image table and made the association and user bootstrap to generate decent styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422100009) do
+ActiveRecord::Schema.define(version: 20160423090549) do
+
+  create_table "images", force: :cascade do |t|
+    t.string   "name"
+    t.string   "picture"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "payments", force: :cascade do |t|
     t.string   "email"
