359e5b105f991e43ab303048b5486959e6a791c5
pouyakhast <pkhast@yahoo.com>
2016-05-08 15:53:19 +0000

    new production path

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,14 @@ class Image < ActiveRecord::Base
   
   belongs_to :user
   
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },   
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :path => ":attachment/:id/:style.:extension",
+    :bucket => "ephotonowapp"
+  
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  
+
 
 end
